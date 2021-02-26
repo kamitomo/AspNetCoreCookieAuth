9081340fc3aa14aa0f49bce18427d8c21d054384
tomohiro8 <h.kamitomo@me.com>
2021-02-26 23:24:48 +0900

    ビューモデルの表示名をフォームラベルに反映

--- AspNetCoreCookieAuth/Models/RegisterUser.cs
+++ AspNetCoreCookieAuth/Models/RegisterUser.cs
@@ -13,6 +13,7 @@ namespace AspNetCoreCookieAuth.Models
         /// </summary>
         [Required]
         [MaxLength(30)]
+        [Display(Name = "ユーザー名")]
         public string UserId { get; set; }
 
         /// <summary>
@@ -20,6 +21,7 @@ namespace AspNetCoreCookieAuth.Models
         /// </summary>
         [Required]
         [MaxLength(100), MinLength(6)]
+        [Display(Name = "パスワード")]
         public string Password { get; set; }
 
         /// <summary>
@@ -27,11 +29,13 @@ namespace AspNetCoreCookieAuth.Models
         /// </summary>
         [Required]
         [MaxLength(100), MinLength(6)]
+        [Display(Name = "確認用パスワード")]
         public string ConfirmPassword { get; set; }
 
         /// <summary>
         /// 管理者
         /// </summary>
+        [Display(Name = "管理者")]
         public bool Admin { get; set; }
     }
 }
