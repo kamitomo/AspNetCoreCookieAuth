5b29a905af4252aec82545c644d018723a4a629c
tomohiro8 <h.kamitomo@me.com>
2021-02-27 01:38:58 +0900

    ユーザビューモデルの検証属性を編集

--- AspNetCoreCookieAuth/Models/RegisterUser.cs
+++ AspNetCoreCookieAuth/Models/RegisterUser.cs
@@ -11,24 +11,27 @@ namespace AspNetCoreCookieAuth.Models
         /// <summary>
         /// ユーザ ID
         /// </summary>
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "{0} の入力が必要です。")]
+        [MaxLength(30, ErrorMessage = "{0} の最大文字数は {1} です。")]
+        [RegularExpression(@"[a-zA-Z0-9\.\-_]+", ErrorMessage = "{0} に使えるのは、半角英数字、ドット(.)、ハイフン(-)、アンダースコア(_)のみです。")]
         [Display(Name = "ユーザー名")]
         public string UserId { get; set; }
 
         /// <summary>
         /// パスワード
         /// </summary>
-        [Required]
-        [MaxLength(100), MinLength(6)]
+        [Required(ErrorMessage = "{0} の入力が必要です。")]
+        [MaxLength(100, ErrorMessage = "{0} の最大文字数は {1} です。"), MinLength(6, ErrorMessage = "{0} の最小文字数は {1} です。")]
+        [RegularExpression(@"[!-~]+", ErrorMessage = "{0} に使えるのは、半角英数字記号のみです。")]
         [Display(Name = "パスワード")]
         public string Password { get; set; }
 
         /// <summary>
         /// 確認用パスワード
         /// </summary>
-        [Required]
-        [MaxLength(100), MinLength(6)]
+        [Required(ErrorMessage = "{0} の入力が必要です。")]
+        [MaxLength(100, ErrorMessage = "{0} の最大文字数は {1} です。"), MinLength(6, ErrorMessage = "{0} の最小文字数は {1} です。")]
+        [RegularExpression(@"[!-~]+", ErrorMessage = "{0} に使えるのは、半角英数字記号のみです。")]
         [Display(Name = "確認用パスワード")]
         public string ConfirmPassword { get; set; }
 
